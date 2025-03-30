1716b15db2f11424442dea4dd9421939716bc3c6
Max Rataj <maxmilianrataj@gmail.com>
2025-03-30 23:12:17 +0200

    Add nest to docker compose, add database connection

--- www/src/app/app.module.ts
+++ www/src/app/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {CitiesModule} from "../cities/cities.module";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {ConfigModule} from "@nestjs/config";
+import {City} from "../cities/city.entity";
+import {Site} from "../sites/Entities/site.entity";
 
 @Module({
-  imports: [],
+  imports: [
+      ConfigModule.forRoot(),
+      CitiesModule,
+      TypeOrmModule.forRoot({
+          type: 'mysql',
+          host: 'explore_mysql',
+          port: 3306,
+          username: 'root',
+          password: 'password',
+          database: 'explore_city_db',
+          entities: [City, Site],
+          synchronize: true,
+      })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
